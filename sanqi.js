d445cdc3a35644f80d04c63dd111d35fab289f2b
三七 <jady1257984872@gmail.com>
2026-04-28 11:01:03 +0800

    chore: update BiliBili Office proxy server IP to 10.23.6.104

--- sanqi.js
+++ sanqi.js
@@ -29,7 +29,7 @@ function main(config) {
   }
   config['proxies'].push({
     name: 'BiliBili Office',
-    server: '10.23.6.35',
+    server: '10.23.6.104',
     port: 8080,
     type: "http"
   })
